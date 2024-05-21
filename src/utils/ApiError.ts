2d0ad7a214b0c25abc6e98d68f15ae530af8b475
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-05-21 01:29:20 +0100

    feat:  API error handler

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,32 @@
+type ApiErrorDetail = {
+  field?: string;
+  message: string | string;
+};
+class ApiError extends Error {
+  statusCode: number;
+  data: null;
+  success: boolean;
+  errors: ApiErrorDetail[];
+
+  constructor(
+    statusCode: number,
+    message = "something went wrong",
+    errors = [],
+    stack = ""
+  ) {
+    super(message);
+    this.statusCode = statusCode;
+    this.data = null;
+    this.message = message;
+    this.success = false;
+    this.errors = errors;
+
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default ApiError;
