b61a8ac936b2e1b5d9f58c5d2214a70c982e4e23
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-05-21 01:28:54 +0100

    feat: authentication middleware

---
+++ src/middlewares/auth.middlewares.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { User } from "../entities/User.entity";
+import { JWT_SECRET } from "../utils/constants";
+
+export const authMiddleware = async (
+  req: Request & { user?: User },
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).send({ message: "No token provided" });
+  }
+ 
+  try {
+    const decodedToken = jwt.verify(token, JWT_SECRET) as {
+      id: string;
+    };
+
+    const user = await User.findOne({
+      where: { id: decodedToken.id },
+    });
+
+    if (!user) {
+      return res.status(401).send({ message: "Invalid token" });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    res.status(400).json({ message: "Invalid token" });
+  }
+};
