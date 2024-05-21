61cf793e1d7a6ad62305a16167e31666b046ce77
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-05-21 23:31:52 +0100

    feat: blockchain service to handle listening to events

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config();
 
+const ETH_RPC_URL = 'https://eth.public-rpc.com';
 const JWT_SECRET = process.env.JWT_SECRET as string;
 const JWT_SECRET_EXPIRY = process.env.JWT_SECRET_EXPIRY;
 const PORT = process.env.PORT;
-const ETH_RPC_URL = process.env.ETH_RPC_URL;
+
 
 const DB_HOST = process.env.DB_HOST;
 const DB_PORT = process.env.DB_PORT;
