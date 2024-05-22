403cd40a5e2a6e78cae768bbd30838f65aa8b2b1
Mbazu Daniel <mbazudaniel97@gmail.com>
2024-05-22 18:48:13 +0100

    feat: dockerizing our application

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM node:18
+
+# Install pnpm
+RUN npm install -g pnpm
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package*.json ./
+COPY pnpm-lock.yaml ./
+RUN pnpm install
+
+# Bundle app source
+COPY . .
+
+# Build the application
+RUN pnpm run build
+
+# Expose the port
+EXPOSE 3000
+
+# Start the application
+CMD ["pnpm", "run", "start"]
--- src/db/data-source.ts
+++ src/db/data-source.ts
@@ -1,21 +1,24 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { join } from "path";
 import {
   DB_HOST,
   DB_DATABASE,
   DB_USERNAME,
   DB_PASSWORD,
   DB_PORT,
+  DB_TYPE,
 } from "../utils/constants";
+
 const AppDataSource = new DataSource({
-  type: "postgres",
+  type: DB_TYPE as any,
   host: DB_HOST,
   port: parseInt(DB_PORT || "5432"),
   username: DB_USERNAME,
   password: DB_PASSWORD,
   database: DB_DATABASE,
-  entities: ["src/entities/*.ts"],
-  synchronize: true,
+  entities: [join(__dirname, "**", "*.entity.{ts,js}")],
+  synchronize: false,
   logging: false,
 });
 
